b18157e0c56522423c69b8c7987e795610942ead
leapingturtlefrog <alexanderaridgides@gmail.com>
2025-02-09 09:29:18 -0500

    rename to Y-M-D

--- ratingAnalysis/calculateStats.py
+++ ratingAnalysis/calculateStats.py
@@ -1,23 +1,38 @@
 import ast
 import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+year = sys.argv[1]
 rating = int(sys.argv[2])
-with open('data' + sys.argv[1] + '.txt') as f:
+with open('data' + year + '.txt') as f:
     data = ast.literal_eval(f.read())
 minimum = min(data.keys())
 maximum = max(data.keys())
-arr = [0] * (maximum - minimum + 1)
-totals = {
-    'users at or below': 0,
-    'users above': 0,
-    'total users': 0
-}
+total_users = sum(data.values())
+arr, sums_proportion = [0] * (maximum - minimum + 1), [0] * (maximum - minimum + 1)
+
 for pair in zip(data.keys(), data.values()):
     arr[pair[0]] = pair[1]
+
+curr_total = 0
 for i in range(len(arr)):
-    totals['total users'] += arr[i]
-    if i <= rating:
-        totals['users at or below'] += arr[i]
-    else:
-        totals['users above'] += arr[i]
-print(totals)
+    curr_total += arr[i]
+    sums_proportion[i] = curr_total / total_users
+users_at_or_below = int(sums_proportion[rating - minimum - 1] * total_users)
+
+print('year:', year)
+print('rating:', rating, '\n')
+
+print('total users:', total_users)
+print('users at or below:', users_at_or_below)
+print('users above:', total_users - users_at_or_below)
+print('percentile:', users_at_or_below / total_users * 100)
+
+plt.plot(
+    [x for x in range(minimum, maximum + 1)],
+    sums_proportion
+)
+plt.show()
+
+sns.plot()
